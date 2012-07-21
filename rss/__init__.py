2acbbc7a34c9dc5bfb3b3408669ee7c7a0240d95
Josh Kunz <joshkunz@me.com>
2012-07-21 00:24:10 -0700

    / redirects to github page

--- rss/__init__.py
+++ rss/__init__.py
@@ -3,7 +3,7 @@ Various rss scripts to read and modify rss feeds,
 collect other information into rss feeds, or modify rss feed contents.
 """
 
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 app.debug = True
@@ -15,6 +15,10 @@ app.secret_key = config.secret
 # import plugins
 from rss.tumblr import tumblr
 from rss.gawker import gawker
+
+@app.route("/")
+def home_page():
+	return redirect("http://github.com/Joshkunz/rss-utils")
 
 app.register_blueprint(tumblr, url_prefix="/tumblr")
 app.register_blueprint(gawker, url_prefix="/gawker")
